ce752d9d68c8eb8dd2f3544fd29f3711a679ad58
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-20 14:05:41 +0530

    ADDED: Insert data from last

--- src/com/bridgelabz/LinkedListDemo.java
+++ src/com/bridgelabz/LinkedListDemo.java
@@ -1,15 +1,6 @@
 package com.bridgelabz;
 
-public class LinkedListDemo {
-
-    public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList();
-        linkedList.insertFirst(70);
-        linkedList.insertFirst(30);
-        linkedList.insertFirst(56);
-        linkedList.showList();
-    }
-}
+import java.util.Scanner;
 
 class Node {
     int data;
@@ -27,10 +18,13 @@ class LinkedList {
     Node tail;
 
     //Adding Node to LinkedList
+    /* 1. Allocate the Node from starting  &
+           2. Put in the data
+           3. Set next as null */
     public void insertFirst(int data) {
         Node newNode = new Node(data);
         //Linked List is Empty
-        if(head == null) {
+        if (head == null) {
             head = newNode;
             tail = newNode;
         } else {
@@ -40,22 +34,65 @@ class LinkedList {
     }
 
     public void showList() {
-        if(head == null) {
+        if (head == null) {
             System.out.println("Linked List is Empty.");
         } else {
             Node temp = head;
-            while(temp != null) {
-                System.out.print(temp.data +"->");
+            while (temp != null) {
+                System.out.print(temp.data + " ");
                 temp = temp.next;
             }
         }
     }
-    public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList();
-        linkedList.insertFirst(70);
-        linkedList.insertFirst(30);
-        linkedList.insertFirst(56);
-        linkedList.showList();
+    /* 1. Allocate the Node from last &
+           2. Put in the data
+           3. Set next as null */
+
+    //Insert Data from Last
+    public void insertLast(int data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
     }
 }
+
+    public class LinkedListDemo {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            LinkedList linkedList = new LinkedList();
+            int choice;
+            do {
+                System.out.println("\n 1.Insert Data from Start \n 2.Insert Data from last \n 3.Show Linked List \n 4.Quit");
+                choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter the number");
+                        int firstnumber = sc.nextInt();
+                        linkedList.insertFirst(firstnumber);
+                        break;
+                    case 2:
+                        System.out.println("Enter the number");
+                        int lastnumber = sc.nextInt();
+                        linkedList.insertLast(lastnumber);
+                        break;
+                    case 3:
+                        linkedList.showList();
+                        break;
+                    case 4:
+                        System.out.println("Thank you");
+                        break;
+                }
+            } while (choice != 4);
+
+        }
+    }
+
+
 
